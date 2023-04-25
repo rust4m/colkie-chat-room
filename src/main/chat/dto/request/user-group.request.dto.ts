53cd8da67b920b67b5d0b2dd7d5e6890e463015d
islamovrn <rustam.islamov@kapitalbank.az>
2023-04-25 15:10:25 +0400

    added swagger documentation

--- src/main/chat/dto/request/user-group.request.dto.ts
+++ src/main/chat/dto/request/user-group.request.dto.ts
@@ -1,10 +1,13 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString, MaxLength } from 'class-validator';
 
 export class UserGroupRequestDto {
+    @ApiProperty({ required: true })
     @IsString()
     @IsNotEmpty()
     readonly userId: string;
 
+    @ApiProperty({ required: true })
     @IsString()
     @IsNotEmpty()
     readonly groupId: string;
--- src/main/chat/dto/request/user.request.dto.ts
+++ src/main/chat/dto/request/user.request.dto.ts
@@ -1,11 +1,14 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsArray, IsNotEmpty, IsString, MaxLength } from 'class-validator';
 
 export class UserRequestDto {
+    @ApiProperty({ required: true })
     @IsString()
     @IsNotEmpty()
     @MaxLength(20)
     readonly fullName: string;
 
+    @ApiProperty({ required: true })
     @IsArray()
     @IsNotEmpty()
     readonly groupIds: string[];
